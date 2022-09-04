5e1b98ab7f642179ad24d787fe1c4984b288c590
neokeee <neopkr90@gmail.com>
2022-09-03 22:45:45 -0400

    changes

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
-from flask import Flask
-from views import views
+from flask import Flask, render_template, request
+import RealmEyeAPI.api as realmdata
+import json
 
 app = Flask(__name__)
-app.register_blueprint(views, url_prefix='/')
+
+@app.route('/')
+def index():
+    return render_template('index.html', ign='')
+@app.route("/", methods=['POST'])
+def home():
+    player = request.form['ign']
+    data = realmdata.ReturnRealmEyeData(realmdata.getRealmEyeData(player), realmdata.getCharacters(player))
+    return render_template('index.html', button=True, ign=json.dumps(data, indent=3))
+@app.route("/data")
+def rawContent():
+    args = request.args
+    name = args.get('player')
+    data = realmdata.ReturnRealmEyeData(realmdata.getRealmEyeData(name), realmdata.getCharacters(name))
+    return render_template('raw.html', ign=json.dumps(data, indent=4))
 
